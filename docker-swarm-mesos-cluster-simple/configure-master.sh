3279fa59acdfec055750abee4511038636a01845
Ross Gardler <rogardle@microsoft.com>
2015-08-29 15:14:30 -0700

    Specificy name of cluster. Make the scripts a little more structured and readable.

--- docker-swarm-mesos-cluster-simple/configure-master.sh
+++ docker-swarm-mesos-cluster-simple/configure-master.sh
@@ -1,30 +1,53 @@
+####################
+# Configure a Master
+####################
+
 # The VM Name for the master should be passed in as a parameter
 masterVMName=$1
 
-# Setup
+#################
+# Install Mesos
+#################
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 
-# Add the repository
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | \
-  sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo tee /etc/apt/sources.list.d/mesosphere.list
 sudo apt-get -y update
 
-# Install mesos
 sudo apt-get -y install mesos
 
-# Configure zk
-echo 1 | sudo tee /etc/zookeeper/conf/myid
-sudo service zookeeper restart
+#################
+# Configure Mesos
+#################
 
-# Stop and disable mesos-slave 
+# Disable mesos-slave on this node
 sudo service mesos-slave stop
 echo manual | sudo tee /etc/init/mesos-slave.override
 
+# Set the master Zookeeper
+echo zk://masterVMName:2181/mesos | sudo tee /etc/mesos/zk
 
+# Specify a human readable name for the Cluster
+echo "ACS-"$masterVMName | sudo tee /etc/mesos-master/cluster
+
+#####################
+# Configure Zookeeper
+#####################
+
+# each Zookeeper needs to know its position in the quorum
+echo 1 | sudo tee /etc/zookeeper/conf/myid
+
+################
 # Install Docker
+################
+
 wget -qO- https://get.docker.com | sh
+
+#################
+# Configure Docker
+#################
 
 # Run swarm manager container on port 2375 (no auth)
 sudo docker run -d -it -p 2375:2375 -p 3375:3375 swarm manage \
@@ -32,5 +55,6 @@ sudo docker run -d -it -p 2375:2375 -p 3375:3375 swarm manage \
     --cluster-opt mesos.address=0.0.0.0 \
     --cluster-opt mesos.port=3375 $masterVMName:5050
 
-# Restart master process
+# Restart master processes
+sudo service zookeeper restart
 sudo service mesos-master restart
--- docker-swarm-mesos-cluster-simple/configure-node.sh
+++ docker-swarm-mesos-cluster-simple/configure-node.sh
@@ -1,37 +1,49 @@
+##################
+# Configure a Node
+##################
+
 # The VM Name for the master should be passed in as a parameter
 masterVMName=$1
 
-# Setup
+##################
+# Install Mesos
+##################
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 
-# Add the repository
 echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | \
   sudo tee /etc/apt/sources.list.d/mesosphere.list
 sudo apt-get -y update
 
-# Install mesos
 sudo apt-get -y install mesos
+
+##################
+# Configure Mesos
+##################
+
+# Stop and disable mesos-master
+sudo service mesos-master stop
+echo manual | sudo tee /etc/init/mesos-master.override
+
+# Add docker containerizer
+echo "docker,mesos" | sudo tee /etc/mesos-slave/containerizers
+
+# Configure zk
+echo "zk://$masterVMName:2181/mesos" | sudo tee /etc/mesos/zk
 
 # Disable and stop zookeeper
 sudo service zookeeper stop
 echo manual | sudo tee /etc/init/zookeeper.override
 
-# Configure zk
-echo "zk://$masterVMName:2181/mesos" | sudo tee /etc/mesos/zk
-
-# Stop and disable mesos-master
-sudo service mesos-master stop
-echo manual | sudo tee /etc/init/mesos-master.override
+##################
+# Configure Docker
+##################
 
 # Install docker and listen on :2375 (no auth, but in vnet)
 wget -qO- https://get.docker.com | sh
 echo 'DOCKER_OPTS="-H unix:// -H 0.0.0.0:2375"' | sudo tee /etc/default/docker
 sudo service docker restart
 
-# Add docker containerizer
-echo "docker,mesos" | sudo tee /etc/mesos-slave/containerizers
-
-# Restart slave process
+# Restart node process
 sudo service mesos-slave restart
