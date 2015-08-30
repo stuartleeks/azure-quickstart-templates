78dc7f696e10798f5679a201f51d91c3809f2dd6
Ross Gardler <rogardle@microsoft.com>
2015-08-30 01:25:26 -0700

    turns out ARM can't run two script extensions, so can't parallelize in this way

--- docker-swarm-mesos-cluster-simple/configure-master.sh
+++ docker-swarm-mesos-cluster-simple/configure-master.sh
@@ -7,6 +7,25 @@ masterVMName=$1
 # The Cluster Name should be passed in as the second parameter
 clusterName=$2
 
+##################
+# Install Mesos
+##################
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" | \
+  sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+sudo apt-get -y install mesos
+
+################
+# Install Docker
+################
+
+wget -qO- https://get.docker.com | sh
+
 #################
 # Configure Mesos
 #################
